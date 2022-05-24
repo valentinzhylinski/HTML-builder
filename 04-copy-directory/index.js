2d6b9d98f1bcef0d2081091062b33cf07be670c7
Valentin Zhylinski <zhylinskivalentin@gmail.com>
2022-05-25 00:23:12 +0300

    fix: 4th task is done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -12,27 +12,25 @@ fs.mkdir(
   }
 );
 
-fs.readdir(filesCopy, (err, files) => {
+fs.readdir(filesCopy, (err, elements) => {
   if (err) throw err;
 
-  for (const file of files) {
-    fs.unlink(path.join(filesCopy, file), (err) => {
+  for (const el of elements) {
+    fs.unlink(path.join(filesCopy, el), (err) => {
       if (err) throw err;
     });
   }
 });
 
 fs.readdir(files, (err, elements) => {
-  if (err) {
-    throw err;
-  } else {
-    elements.map((el) => {
-      fs.copyFile(`${files}/${el}`, `${filesCopy}/${el}`, (err) => {
-        if (err) {
-          throw err;
-        }
-      });
+  if (err) throw err;
+
+  for (const el of elements) {
+    fs.copyFile(`${files}/${el}`, `${filesCopy}/${el}`, (err) => {
+      if (err) {
+        throw err;
+      }
     });
-    console.log("Copied succesfully!");
   }
+  console.log("Copied succesfully!");
 });
