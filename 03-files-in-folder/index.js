03838e99cb65d9adaf5fddfc256470340fe8948a
Valentin Zhylinski <zhylinskivalentin@gmail.com>
2022-05-25 00:25:58 +0300

    fix: 3rd task is done

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,23 +4,21 @@ const path = require("path");
 const secretFolder = `${path.join(__dirname, "./secret-folder")}`;
 
 fs.readdir(secretFolder, { withFileTypes: true }, (err, elements) => {
-  if (err) {
-    throw err;
-  } else {
-    elements.map((el) => {
-      if (el.isFile()) {
-        fs.stat(`${secretFolder}/${el.name}`, (err, stats) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(
-              `${path.parse(el.name).name} - ${path
-                .parse(el.name)
-                .ext.slice(1)} - ${stats.size / 1000}kb`
-            );
-          }
-        });
-      }
-    });
+  if (err) throw err;
+
+  for (const el of elements) {
+    if (el.isFile()) {
+      fs.stat(`${secretFolder}/${el.name}`, (err, stats) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(
+            `${path.parse(el.name).name} - ${path
+              .parse(el.name)
+              .ext.slice(1)} - ${stats.size / 1000}kb`
+          );
+        }
+      });
+    }
   }
 });
