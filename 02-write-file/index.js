9e0b90d122644472dfaba1f20ba388d8a60884af
Valentin Zhylinski <zhylinskivalentin@gmail.com>
2022-05-24 17:17:36 +0300

    add: 2nd task is done i suppose

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const greeting = "Hello, enter your text below!";
+const goodbye = "Bye, have a nice day";
+
+const writeStream = fs.createWriteStream(path.join(__dirname, "input.txt"), {
+  flags: "a",
+});
+
+console.log(greeting);
+
+readline.on("line", (input) => {
+  let exitText = input.toLowerCase();
+  if (exitText === "exit") {
+    readline.close();
+  } else {
+    writeStream.write(input + "\n");
+  }
+});
+
+readline.on("SIGINT", () => {
+  readline.close();
+});
+
+readline.on("close", () => {
+  console.log(goodbye);
+});
