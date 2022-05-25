9c778f2b08e43ad473ba60d62623dd71bd1981bd
Valentin Zhylinski <zhylinskivalentin@gmail.com>
2022-05-25 20:12:00 +0300

    add: solution wth order

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
+const styles = `${path.join(__dirname, "/styles")}`;
+const bundle = `${path.join(__dirname, "/project-dist/bundle.css")}`;
+const projectDist = `${path.join(__dirname, "project-dist")}`;
+
+fs.readdir(projectDist, (err, elements) => {
+  if (err) throw err;
+
+  for (const el of elements) {
+    if (el === "bundle.css") {
+      fs.unlink(bundle, (err) => {
+        if (err) throw err;
+      });
+    }
+  }
+});
+
+fs.readdir(styles, (err, elements) => {
+  if (err) throw err;
+
+  for (const el of elements) {
+    if (path.parse(el).ext === ".css") {
+      let fileContent = fs.createReadStream(
+        `${path.join(styles, el)}`,
+        "utf-8"
+      );
+      fileContent.on("data", (content) => {
+        let writeStream = fs.createWriteStream(bundle, { flags: "a" });
+        writeStream.write(`${content}\n`);
+      });
+    }
+  }
+});
